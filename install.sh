3825164b6c50dd4ec4ec89ac6e598a177944c0aa
Ivan <anilihst@gmail.com>
2013-07-08 21:50:25 +0200

    Made the install script more robust

--- install.sh
+++ install.sh
@@ -3,16 +3,22 @@
 #Exit on error
 set -e
 
+num=1
+if [ -n "$1" ]
+then
+	num="$1"
+fi
 
-device_found=$(adb devices | grep 'device$')
-if [ -z "$(adb devices | grep 'device')" ]; then
+if [ -z "$(adb devices | grep 'device$')" ]
+then
 	echo "Device not found"
 	exit 1
 fi
+device_found=$(adb devices | grep 'device$' | sed -e 's/\ *device//' | head -n $num)
 echo "device found: "$device_found
 
 echo "Attempting to uninstall previous versions"
-adb uninstall org.servalproject
+adb -s $device_found  uninstall org.servalproject
 
 echo "Installing new version"
-adb install serval/bin/batphone-debug.apk
+adb -s $device_found install serval/bin/batphone-debug.apk
